d1fbacc022f231572e55be0ca687d3ccd197c9e5
shubham-joshi29 <shubhamsjoshi29@gmail.com>
2025-02-28 17:04:07 +0530

    component scan changes

--- User/src/main/java/secondApp/EcomService.java
+++ User/src/main/java/secondApp/EcomService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EcomService {
 	public void marathi() {
-		System.out.println("this is extra package");
+		System.out.println("this is extra package to test component scan annotation");
 	}
 
 }
