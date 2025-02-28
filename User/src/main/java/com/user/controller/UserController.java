b1e1b9e40b358ca2d96ed621f79327f7588a88d3
shubham-joshi29 <shubhamsjoshi29@gmail.com>
2025-02-28 23:15:36 +0530

    getUsermethod add in controller

--- User/src/main/java/com/user/controller/UserController.java
+++ User/src/main/java/com/user/controller/UserController.java
@@ -59,6 +59,12 @@ public class UserController {
 		Integer id = userService.removeUser(userId);
 		return new ResponseEntity<>(id,HttpStatus.OK);
 	}
+	@GetMapping("/getUsers")
+	public ResponseEntity<List<UserDTO>> getUsers(){
+		List<UserDTO> users = userService.getAllUsers();
+		return new ResponseEntity<>(users,HttpStatus.OK);
+	}
+	
 	
 
 }
